be85be01c02a0ad9fe9520b431c8874b57524fb7
Frank Ures <frank.ures12@gmail.com>
2025-08-06 15:47:34 -0700

    cors fix

--- Backend/app.py
+++ Backend/app.py
@@ -22,7 +22,7 @@ else:
     print("OpenAI version is compatible")
 
 app = Flask(__name__)
-CORS(app, origins=["https://mywaterwarehouse.com", "https://wordpress-984626-5694127.cloudwaysapps.com"])
+CORS(app)#origins=["https://mywaterwarehouse.com", "https://wordpress-984626-5694127.cloudwaysapps.com"])
  
 client = OpenAI(api_key=OPENAI_API_KEY)
 assistant_id = functions.create_assistant(client)
